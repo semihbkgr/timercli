0832227528cbe97388e3431f537e6d285261e9c4
SemihBKGR <buyukgungorsemih@gmail.com>
2022-09-01 23:54:35 +0000

    countdown start operation and remaining duration

--- cmd.go
+++ cmd.go
@@ -12,7 +12,6 @@ func main() {
 	d, err := parseDuration()
 	checkErr(err)
 	c := newCountdown(d)
-	c.start()
 	c.wait()
 }
 
@@ -21,7 +20,18 @@ func parseDuration() (time.Duration, error) {
 	if len(args) == 0 {
 		return 0, errors.New("missing duration arg")
 	}
-	return time.ParseDuration(args[0])
+	d, err := time.ParseDuration(args[0])
+	if err != nil {
+		return 0, err
+	}
+	return validateDuration(d)
+}
+
+func validateDuration(d time.Duration) (time.Duration, error) {
+	if d < 0 {
+		return 0, errors.New("non-positive duration")
+	}
+	return d, nil
 }
 
 func checkErr(err error) {
--- countdown.go
+++ countdown.go
@@ -2,26 +2,50 @@ package main
 
 import "time"
 
+const defaultTickRate = 100 * time.Millisecond
+
 type countdown struct {
-	duration time.Duration
-	tickRate time.Duration
-	ticker   *time.Ticker
+	duration  time.Duration
+	tickRate  time.Duration
+	ticker    *time.Ticker
+	startTime time.Time
+	chans     []chan time.Duration
 }
 
 func newCountdown(d time.Duration) *countdown {
-	return &countdown{
-		duration: d,
-		tickRate: 100 * time.Millisecond,
-		ticker:   time.NewTicker(d),
+	c := &countdown{
+		duration:  d,
+		tickRate:  defaultTickRate,
+		ticker:    time.NewTicker(defaultTickRate),
+		startTime: time.Now(),
+		chans:     make([]chan time.Duration, 0),
 	}
+	go startCountdown(c)
+	return c
 }
 
-func (c *countdown) start() {
-	go func() {
-
-	}()
+func (c *countdown) remaining() chan<- time.Duration {
+	ch := make(chan time.Duration, 0)
+	c.chans = append(c.chans, ch)
+	return ch
 }
 
-func (c *countdown) wait() {
-	<-c.ticker.C
+func startCountdown(c *countdown) {
+	for {
+		t, ok := <-c.ticker.C
+		if !ok {
+			for _, ch := range c.chans {
+				close(ch)
+			}
+			return
+		}
+		r := c.duration - time.Duration(t.UnixNano()-c.startTime.UnixNano())
+		if r < 0 {
+			c.ticker.Stop()
+			continue
+		}
+		for _, ch := range c.chans {
+			ch <- r
+		}
+	}
 }
