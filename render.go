15892c54fbc1e51ddf27ef28879f134020dedbf4
semihbkgr <buyukgungorsemih@gmail.com>
2022-09-04 00:42:27 +0300

    render text type and refactor font

--- render.go
+++ render.go
@@ -5,107 +5,152 @@ import (
 	"strings"
 )
 
+const textCharHeight = 5
+
 const (
 	zero = `
-######.
-#....#.
-#....#.
-#....#.
-######.
+######
+#    #
+#    #
+#    #
+######
 `
 	one = `
-.....#.
-.....#.
-.....#.
-.....#.
-.....#.
+     #
+     #
+     #
+     #
+     #
 `
 	two = `
-######.
-.....#.
-######.
-#......
-######.
+######
+     #
+######
+#
+######
 `
 	three = `
-######.
-.....#.
-...###.
-.....#.
-######.
+######
+     #
+   ###
+     #
+######
 `
 	four = `
-#......
-#......
-#...#..
-######.
-....#..
+#
+#
+#   #
+######
+    #
 `
 	five = `
-######.
-#......
-######.
-.....#.
-######.
+######
+#
+######
+     #
+######
 `
 	six = `
-######.
-#......
-######.
-#....#.
-######.
+######
+#
+######
+#    #
+######
 `
 	seven = `
-######.
-.....#.
-.....#.
-.....#.
-.....#.
+######
+     #
+     #
+     #
+     #
 `
 	eight = `
-######.
-#....#.
-######.
-#....#.
-######.
+######
+#    #
+######
+#    #
+######
 `
 	nine = `
-######.
-#....#.
-######.
-.....#.
-######.
+######
+#    #
+######
+     #
+######
 `
 	column = `
-..
-#.
-..
-#.
-..
+
+##
+
+##
+
+`
+	cross = `
+##   ##
+ ## ##
+  ###
+ ## ##
+##   ##
 `
 )
 
-var numbers = [10][][]rune{
-	stringToRuneMatrix(one),
-	stringToRuneMatrix(two),
-	stringToRuneMatrix(three),
-	stringToRuneMatrix(four),
-	stringToRuneMatrix(five),
-	stringToRuneMatrix(six),
-	stringToRuneMatrix(seven),
-	stringToRuneMatrix(eight),
-	stringToRuneMatrix(nine),
+type text [][]bool
+
+func (t text) width() int {
+	return len((t)[0])
 }
 
-var separator = stringToRuneMatrix(column)
+var textChars = map[rune]text{
+	'0': stringToTextChar(zero),
+	'1': stringToTextChar(one),
+	'2': stringToTextChar(two),
+	'3': stringToTextChar(three),
+	'4': stringToTextChar(four),
+	'5': stringToTextChar(five),
+	'6': stringToTextChar(six),
+	'7': stringToTextChar(seven),
+	'8': stringToTextChar(eight),
+	'9': stringToTextChar(nine),
+	':': stringToTextChar(column),
+	'x': stringToTextChar(cross),
+}
 
-func stringToRuneMatrix(s string) [][]rune {
+func stringToTextChar(s string) text {
 	lines := strings.Split(s, "\n")
-	matrix := make([][]rune, len(lines))
+	matrix := make([][]bool, textCharHeight)
+	var width int
+	for _, line := range lines {
+		if l := len(line); width < l {
+			width = l
+		}
+	}
 	for y, line := range lines {
-		matrix[y] = []rune(line)[0 : len(line)-1]
+		array := make([]bool, width)
+		for x := 0; x < width; x++ {
+			array[x] = x < len(line) && line[x] == '#'
+		}
+		matrix[y] = array
 	}
 	return matrix
+}
+
+func concatTexts(t ...text) text {
+	ct := make(text, textCharHeight)
+	var width int
+	for _, c := range t {
+		width += c.width()
+	}
+	for y := 0; y < len(ct); y++ {
+		line := make([]bool, width)
+		x := 0
+		for _, c := range t {
+			for _, b := range c[y] {
+				line[x] = b
+				x++
+			}
+		}
+	}
+	return ct
 }
 
 func startTermbox() {
@@ -146,6 +191,6 @@ func NewRenderer(theme string) *Renderer {
 	}
 }
 
-func (r *Renderer) Render(i ...int) {
+func (r *Renderer) Render(s string) {
 	termbox.Clear(r.t.bg, r.t.bg)
 }
