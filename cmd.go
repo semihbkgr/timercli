69219a72c2bd55d217f68f6aeb59001f8915bb78
semihbkgr <buyukgungorsemih@gmail.com>
2022-09-09 03:14:15 +0300

    adjustment of arg parsing and exit status code

--- cmd.go
+++ cmd.go
@@ -17,16 +17,13 @@ var theme = commandLine.String("t", "light", "theme of the renderer on the conso
 
 //todo: interrupted and proceed signal
 //todo: print elapsed time at the end
-//todo: error when duration is 0
 func main() {
 	defer handleError()
 	initTermbox()
 	defer closeTermbox()
-	d, err := parseDuration()
-	checkErr(err)
+	d, ok := parseDuration()
 	var t Timer
-	if d != 0 {
-		checkErr(validateDuration(d))
+	if ok {
 		t = NewCountdown(d)
 	} else {
 		t = NewChronometer()
@@ -53,19 +50,21 @@ func commandLineArgs() []string {
 	return commandLine.Args()
 }
 
-func parseDuration() (time.Duration, error) {
+func parseDuration() (time.Duration, bool) {
 	args := commandLineArgs()
-	if len(args) == 0 {
-		return 0, nil
+	if l := len(args); l == 0 {
+		return 0, false
+	} else if l > 1 {
+		panic(errors.New("too many arguments"))
 	}
-	return time.ParseDuration(args[0])
-}
-
-func validateDuration(d time.Duration) error {
-	if d < 0 {
-		return errors.New("non-positive duration")
+	d, err := time.ParseDuration(args[0])
+	if err != nil {
+		panic(err)
 	}
-	return nil
+	if d < 1 {
+		panic(errors.New("non-positive duration"))
+	}
+	return d, true
 }
 
 func checkErr(err error) {
@@ -76,25 +75,29 @@ func checkErr(err error) {
 
 func handleError() {
 	r := recover()
+	status := 0
 	if r != nil {
 		switch t := r.(type) {
 		case error:
-			if flagParseError {
-				commandLine.Usage()
-			}
 			if t != flag.ErrHelp {
 				_, err := fmt.Fprintln(os.Stderr, t.Error())
 				if err != nil {
 					panic(err)
 				}
+				status = 1
+			}
+			if flagParseError {
+				commandLine.Usage()
 			}
 		default:
 			_, err := fmt.Fprintln(os.Stderr, t)
 			if err != nil {
 				panic(err)
 			}
+			status = 1
 		}
 	}
+	os.Exit(status)
 }
 
 func formatDuration(d time.Duration) string {
